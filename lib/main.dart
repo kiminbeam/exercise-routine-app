7c96961b39a2ca96ca6305a13ffb7d9741f74d21
SeeunJoe <whtpdms1556@naver.com>
2025-01-06 18:31:10 +0900

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:projectsampledata/ui/pages/exercise/exercise_list_page/exercise_list_page.dart';
+import 'package:projectsampledata/ui/pages/exercise/exercise_plus_page/add_exercise_detail_page.dart';
 import 'package:projectsampledata/ui/pages/main/main_page/main_page.dart';
+import 'package:projectsampledata/ui/pages/user/plan_update_page/plan_update_page.dart';
+import 'package:projectsampledata/ui/pages/user/user_info_page/user_info_page.dart';
+import 'package:projectsampledata/ui/pages/user/user_info_update_page/user_info_update_page.dart';
+import 'package:projectsampledata/ui/pages/user/user_join_page/join_page.dart';
+import 'package:projectsampledata/ui/pages/user/user_login_page/login_page.dart';
+import 'package:projectsampledata/ui/pages/week/week_detail_page/list_detail_of_day_page.dart';
+
+import '_core/constants/theme.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -8,39 +18,39 @@ void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
-  }
-}
-
-// 라우팅 필요하면 아래 내용에서 필요한 부분만 알아서 주소 바꾸고 쓰십셔
 // class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       navigatorKey: navigatorKey,
-//       // context가 없는 곳에서 context를 사용할 수 있는 방법
 //       debugShowCheckedModeBanner: false,
-//       //home: SplashPage(),
-//       routes: {
-//         "/login": (context) => LoginPage(), // 로그인 페이지
-//         "/join": (context) => JoinPage(), // 회원가입 페이지
-//         "/exercise-add": (context) => AddExerciseDetailPage(), // 운동 추가 페이지
-//         "/day-exercise": (context) => ListDetailOfDayPage(), // 요일별 운동 상세보기
-//         "/plan-update": (context) => PlanUpdatePage(), // 요일별 계획 업데이트 페이지
-//         "/user-info-update": (context) => UserInfoUpdatePage(), // 유저 정보 수정 페이지
-//         "/main-page": (context) => MainPage(), // 메인 페이지 (월화수목금토일)
-//         "/user-info": (context) => UserInfoPage(), // 유저 정보 페이지
-//         "/exercise-list": (context) => ExerciseListPage(), // 유저 정보 페이지
-//       },
-//       theme: theme(),
+//       home: MainPage(),
 //     );
 //   }
 // }
+
+// 라우팅 필요하면 아래 내용에서 필요한 부분만 알아서 주소 바꾸고 쓰십셔
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      // context가 없는 곳에서 context를 사용할 수 있는 방법
+      debugShowCheckedModeBanner: false,
+      //home: SplashPage(),
+      routes: {
+        "/login": (context) => LoginPage(), // 로그인 페이지
+        "/join": (context) => JoinPage(), // 회원가입 페이지
+        "/exercise-add": (context) => AddExerciseDetailPage(), // 운동 추가 페이지
+        "/day-exercise": (context) => ListDetailOfDayPage(), // 요일별 운동 상세보기
+        "/plan-update": (context) => PlanUpdatePage(), // 요일별 계획 업데이트 페이지
+        "/user-info-update": (context) => UserInfoUpdatePage(), // 유저 정보 수정 페이지
+        "/main-page": (context) => MainPage(), // 메인 페이지 (월화수목금토일)
+        "/user-info": (context) => UserInfoPage(), // 유저 정보 페이지
+        "/exercise-list": (context) => ExerciseListPage(), // 유저 정보 페이지
+      },
+      theme: theme(),
+    );
+  }
+}
