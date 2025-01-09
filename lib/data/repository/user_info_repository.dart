c1e59c99c3be1350beedc4ad382516038b5b9b25
angus <angus1208@naver.com>
2025-01-09 17:17:19 +0900

    [feat] 유저 정보 페이지 데이터바인딩 완료

--- lib/data/repository/user_info_repository.dart
+++ lib/data/repository/user_info_repository.dart
@@ -1,37 +1,13 @@
 import 'package:dio/dio.dart';
 
+import '../../_core/utils/http_util.dart';
+
 class UserInfoRepository {
   const UserInfoRepository();
 
-  Future<Map<String, dynamic>> findById({required int id}) async {
-    Response response = await findUser();
+  Future<Map<String, dynamic>> findById(int id) async {
+    Response response = await dio.get("/api/user/$id");
     Map<String, dynamic> body = response.data;
     return body;
   }
-}
-
-// 아래는 샘플 데이터. 언제든지 변경될 수 있음
-class User {
-  int id;
-  String username;
-  double? height;
-  double? weight;
-
-  User({required this.id, required this.username, this.height, this.weight});
-
-  static fromMap(Map<String, dynamic> map) {}
-}
-
-Future<Response> findUser() async {
-  final user = {"id": 1, "username": "ssar", "weight": 50, "height": 150};
-
-  Map<String, dynamic> responseBody = {
-    'success': true,
-    'response': user,
-  };
-  return Response(
-    requestOptions: RequestOptions(path: '/user/1'),
-    data: responseBody,
-    statusCode: 200,
-  );
 }
