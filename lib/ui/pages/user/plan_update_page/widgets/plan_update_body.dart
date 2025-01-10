67d26a20d00c93803260f5588f1d357fc3231cf3
kib <kods9852@gmail.com>
2025-01-10 14:37:47 +0900

    [feat] 계획수정기능추가

--- lib/ui/pages/user/plan_update_page/widgets/plan_update_body.dart
+++ lib/ui/pages/user/plan_update_page/widgets/plan_update_body.dart
@@ -1,47 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:projectsampledata/ui/pages/user/plan_update_page/plan_update_vm.dart';
+import 'package:projectsampledata/ui/pages/user/plan_update_page/widgets/plan_update_button.dart';
 
-class PlanUpdateBody extends StatelessWidget {
-  final TextEditingController controller;
+class PlanUpdateBody extends ConsumerWidget {
+  final planId;
 
-  final String labelText;
+  PlanUpdateBody({required this.planId});
 
-  final String hintText;
-
-  final IconData icon;
-
-  PlanUpdateBody(
-      {required this.controller,
-      required this.labelText,
-      required this.hintText,
-      required this.icon});
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final sets = TextEditingController();
+  final repeatCount = TextEditingController();
+  final weight = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: labelText,
-        hintText: hintText,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: Colors.blueAccent,
-            width: 2.0,
+  Widget build(BuildContext context, WidgetRef ref) {
+    PlanUpdateVM vm = ref.read(planUpdateProvider.notifier);
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 500,
+              child: Center(
+                child: Text(
+                  "운동 계획 수정",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.grey,
+              ),
+            ),
           ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: Colors.grey,
-            width: 1.0,
+          SizedBox(
+            width: 300,
+            height: 300,
+            child: Image.asset(
+              "assets/강아지_2.jpg",
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        prefixIcon: Icon(icon, color: Colors.grey),
-        fillColor: Colors.white,
-        filled: true,
+          SizedBox(height: 30),
+          PlanUpdateButton(
+            controller: sets,
+            labelText: "세트 수 입력",
+            hintText: "Ex: 3",
+            icon: Icons.edit,
+          ),
+          SizedBox(height: 30),
+          PlanUpdateButton(
+            controller: repeatCount,
+            labelText: "반복 횟수 입력",
+            hintText: "Ex: 12",
+            icon: Icons.repeat,
+          ),
+          SizedBox(height: 30),
+          PlanUpdateButton(
+            controller: weight,
+            labelText: "무게 입력",
+            hintText: "20",
+            icon: Icons.fitness_center,
+          ),
+          SizedBox(height: 30),
+          TextButton(
+            onPressed: () {
+              // vm으로 이동해서 비즈니스 로직 진행 구현해야 됨.
+              vm.updatePlan(planId, sets.text.trim(), repeatCount.text.trim(),
+                  weight.text.trim());
+            },
+            child: Text(
+              "수정하기",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.grey[600],
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+          ),
+        ],
       ),
     );
   }
