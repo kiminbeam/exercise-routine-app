06791b34543c186739dcb4d9545cc6758e423b11
lnk <nakyeom0620@gmail.com>
2025-01-10 18:21:31 +0900

    [feat] 요일 별 운동 리스트에서 운동 삭제 기능 구현

--- lib/data/repository/list_detail_of_day_repository.dart
+++ lib/data/repository/list_detail_of_day_repository.dart
@@ -6,12 +6,23 @@ import 'package:projectsampledata/_core/utils/http_util.dart';
 class ListDetailOfDayInfoRepository {
   const ListDetailOfDayInfoRepository();
 
+  // 요일 별 운동 계획 리스트 가져오도록 서버에 요청
   Future<Map<String, dynamic>> takeListDetailOfDayInformaition(
       String day) async {
     Response response = await dio.get(
       "/api/plan",
       queryParameters: {"day": day},
     );
+
+    Map<String, dynamic> body = response.data;
+
+    Logger().d(body);
+    return body;
+  }
+
+  // 요일 별 운동 계획에서 운동 삭제하도록 서버에 요청
+  Future<Map<String, dynamic>> deletePlan(int id) async {
+    Response response = await dio.delete("/plan/$id/delete");
 
     Map<String, dynamic> body = response.data;
 
