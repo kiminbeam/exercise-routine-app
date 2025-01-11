5d0809a146b290324e19f83b1aac7c0795ef8060
lnk <nakyeom0620@gmail.com>
2025-01-11 23:33:02 +0900

    [feat]요일 별 운동 리스트에서 운동 삭제하기 기능 구현 후 master rebase 재시도

--- test/list_of_day_repository_test.dart
+++ test/list_of_day_repository_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:projectsampledata/data/repository/list_detail_of_day_repository.dart';
 
 void main() {
+  // 운동 삭제 테스트
   test('Test ListDetailOfDayInfoRepository', () async {
     // Arrange
     ListDetailOfDayInfoRepository repo = ListDetailOfDayInfoRepository();
@@ -12,6 +13,6 @@ void main() {
     var result = await repo.deletePlan(id);
 
     // Assert
-    //expect(result, isNotNull); // 원하는 조건에 따라 수정
+    expect(result, isNotNull); // 원하는 조건에 따라 수정
   });
 }
